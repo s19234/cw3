a05219c6545914e3f381e654459aabd5282c5ce9
Ryszard <s19234@pjwskt.edu.pl>
2020-03-30 11:35:54 +0200

    Dodano klasę imitująca baze danych

---
+++ Zadanie3/Zadanie3/DAL/MockDBService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Zadanie3.Models;
+
+namespace Zadanie3.DAL
+{
+    public class MockDBService : IDBService
+    {
+        private static IEnumerable<Student> _students;
+
+        static MockDBService()
+        {
+            _students = new List<Student>
+            {
+                new Student(1, "Jan", "Kowalski"),
+                new Student(2, "Anna", "Malewski"),
+                new Student(3, "Andrzej", "Andrzejewicz")
+            };
+        }
+
+        public IEnumerable<Student> GetStudents()
+        {
+            return _students;
+        }
+    }
+}
