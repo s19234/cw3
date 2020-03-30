5bc27960d943c2a875f32ac8f87c2ff518be90e8
Ryszard <s19234@pjwskt.edu.pl>
2020-03-30 11:08:56 +0200

    Zaimplementowano interfejs IComparable

--- Zadanie3/Zadanie3/Models/Student.cs
+++ Zadanie3/Zadanie3/Models/Student.cs
@@ -5,11 +5,17 @@ using System.Threading.Tasks;
 
 namespace Zadanie3.Models
 {
-    public class Student
+    public class Student : IComparable
     {
         public int IdStudent { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string IndexNumber { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            Student student = obj as Student;
+            return this.IdStudent - student.IdStudent;
+        }
     }
 }
