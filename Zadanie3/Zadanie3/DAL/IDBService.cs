d08394750db7d42880c55a3665804d56f73a5ec7
Ryszard <s19234@pjwskt.edu.pl>
2020-03-30 11:35:17 +0200

    Dodano interfejs IDBService

---
+++ Zadanie3/Zadanie3/DAL/IDBService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Zadanie3.Models;
+
+namespace Zadanie3.DAL
+{
+    public interface IDBService
+    {
+        public IEnumerable<Student> GetStudents();
+    }
+}
