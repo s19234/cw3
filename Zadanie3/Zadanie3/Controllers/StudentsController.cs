b089e4d6589d6b2390e8e9e31847df5925541d3d
Ryszard <s19234@pjwskt.edu.pl>
2020-03-30 10:42:34 +0200

    Initial Commit - zadanie 3

---
+++ Zadanie3/Zadanie3/Controllers/StudentsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Zadanie3.Controllers
+{
+    [Route("api/students")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetStudents()
+        {
+            return "Kowalski, Malewski, Andrzejewski";
+        }
+    }
+}
